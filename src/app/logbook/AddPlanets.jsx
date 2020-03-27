d830b05034f492bf6b11b62cb9fc409a3efb0e67
Isac Petinate <ipetinate@outlook.com>
2020-03-26 23:55:03 -0300

    Componente de fomrulário separado do componente principal

--- src/app/logbook/AddPlanets.jsx
+++ src/app/logbook/AddPlanets.jsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState, createRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 // Material UI
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
-import TextField from '@material-ui/core/TextField'
-import InputLabel from '@material-ui/core/InputLabel'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 import { makeStyles } from '@material-ui/core'
-// React Hook Form
-import { useForm, Controller } from 'react-hook-form'
+// App Components
+import PlanForm from './PlanForm'
 // App Resources
 import LocalStorageService from '../../services/local-storage.service'
-import SwapiService from '../../services/swapi.service'
 // Dictionaries
-import Endpoints from '../../dictionaries/endpoints.dictionary'
 import LOCAL_STORAGE_KEY from '../../dictionaries/local-storage.dictionary'
 
 const useStyles = makeStyles(theme => ({
@@ -34,21 +27,10 @@ const useStyles = makeStyles(theme => ({
 
 export default function AddPlanets () {
   const classes = useStyles()
+
   const localStorageService = new LocalStorageService()
-  const swapiService = new SwapiService()
-
   const KEY = LOCAL_STORAGE_KEY.get('KEY')
-  const ENDPOINT = Endpoints.get('PLANETS')
 
-  const descriptionInput = createRef()
-  const planetSelect = createRef()
-
-  const { register, handleSubmit, control } = useForm()
-
-  const [description, setDescription] = useState({})
-  const [planet, setSelectedPlanet] = useState([])
-
-  const [planets, setPlanets] = useState([])
   const [plans, setPlans] = useState([])
 
   useEffect(() => {
@@ -56,110 +38,52 @@ export default function AddPlanets () {
   }, [])
 
   useEffect(() => {
-    (async () => {
-      const data = await swapiService.getAll(ENDPOINT)
-      setPlanets(data)
-    })()
-  }, [])
+    savePlansLocal(plans)
+  }, [plans])
 
-  useEffect(() => {
-    register({ name: 'planets' }, { required: true })
-    register({ name: 'description', type: 'text' })
-  }, [register])
-
-  const handleSubmitPlan = () => {
-    setPlans([...plans, { description, planet }])
-    handleAddPlan(plans)
-  }
-
-  const handleDeletePlan = (item) => {
+  const deletePlan = (item) => {
     setPlans(plans.filter(x => x !== item))
-    handleAddPlan(plans)
   }
 
-  const handleAddPlan = (plansList) => {
+  const savePlansLocal = (plansList) => {
     if (!plansList || plansList.length > 0) {
-      localStorageService.create(KEY, [...plansList])
+      localStorageService.create(KEY, plansList)
     }
+  }
+
+  const addPlan = (plan) => {
+    setPlans([...plans, plan])
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit(data => console.log(data))}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12}>
-            <Controller
-              as={TextField}
-              fullWidth
-              required
-              autoComplete='off'
-              color='secondary'
-              id='description'
-              label='Descrição'
-              name='description'
-              value={description}
-              inputRef={descriptionInput}
-              onChange={([value]) => setDescription(value)}
-              control={control}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12}>
-            <InputLabel id='planet'>Planeta</InputLabel>
-            <Controller
-              as={
-                <Select
-                  fullWidth
-                  id='planet'
-                  label='Planeta'
-                  labelId='planet'
-                  color='secondary'
-                  value={planet}
-                  inputRef={planetSelect}
-                  onChange={([selected]) => setSelectedPlanet('planet', selected.target.value)}
-                >
-                  {
-                    planets.map((planet, index) =>
-                      <MenuItem key={planet.name} value={planet}>{planet.name}</MenuItem>
-                    )
-                  }
-                </Select>
-              }
-              name='planet'
-              defaultValue=''
-              control={control}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant='contained' color='primary' type='submit'>
-              Adicionar
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+      <PlanForm
+        onSubmit={
+          ({ description, planet }) => addPlan({ description, planet })
+        }
+      />
       <Grid container spacing={3}>
         <Grid item xs={12} sm={12}>
           {
-            plans ? plans.map(
-              (x, index) => (
-                <Paper key={index} className={classes.paper}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={11} sm={11} zeroMinWidth>
-                      <Typography>
-                        Planeta: {x.name}
-                      </Typography>
-                      <Typography>
-                        Descrição: {x.description}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={1} sm={1}>
-                      <IconButton edge='start' color='inherit' aria-label='open drawer'>
-                        <DeleteForeverIcon onClick={() => handleDeletePlan(x)} color='primary' />
-                      </IconButton>
-                    </Grid>
+            plans.map((plan, index) =>
+              <Paper key={index} className={classes.paper}>
+                <Grid container spacing={2}>
+                  <Grid item xs={11} sm={11} zeroMinWidth>
+                    <Typography>
+                      Planeta: {plan.planet.name}
+                    </Typography>
+                    <Typography>
+                      Descrição: {plan.description}
+                    </Typography>
                   </Grid>
-                </Paper>
-              )
-            ) : null
+                  <Grid item xs={1} sm={1}>
+                    <IconButton edge='start' color='inherit' aria-label='open drawer'>
+                      <DeleteForeverIcon onClick={() => deletePlan(plan)} color='primary' />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Paper>
+            )
           }
         </Grid>
       </Grid>
---
+++ src/app/logbook/PlanForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+// React Hook Form
+import { useForm } from 'react-hook-form'
+// Material UI
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
+import InputLabel from '@material-ui/core/InputLabel'
+// App Resources
+import SwapiService from '../../services/swapi.service'
+// Dictionaries
+import Endpoints from '../../dictionaries/endpoints.dictionary'
+
+const PlanForm = ({ onSubmit }) => {
+  const { handleSubmit, register } = useForm()
+
+  const swapiService = new SwapiService()
+  const ENDPOINT = Endpoints.get('PLANETS')
+
+  const [description, setDescription] = useState([])
+  const [planet, setPlanet] = useState([])
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const data = await swapiService.getAll(ENDPOINT)
+      setPlanets(data)
+    })()
+  }, [])
+
+  const addPlan = () => onSubmit({ description, planet })
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(addPlan)}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12}>
+            <TextField
+              fullWidth
+              autoComplete='off'
+              color='secondary'
+              name='description'
+              label='Descrição'
+              ref={register}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <InputLabel id='planet'>Planeta</InputLabel>
+            <Select
+              fullWidth
+              color='secondary'
+              id='planet'
+              name='planet'
+              label='Planeta'
+              labelId='Planet'
+              ref={register}
+              onChange={e => setPlanet(e.target.value)}
+            >
+              {
+                planets.map((planet, index) =>
+                  <MenuItem key={index} value={planet}>{planet.name}</MenuItem>
+                )
+              }
+            </Select>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant='contained' color='primary' type='submit'>
+              Adicionar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </>
+  )
+}
+
+PlanForm.propTypes = {
+  onSubmit: PropTypes.func
+}
+
+export default PlanForm
